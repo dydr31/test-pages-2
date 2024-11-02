2e5e770dbe6ecea8953fc1f3cfd1b0d5eda57cf7
dydr31 <84735554+dydr31@users.noreply.github.com>
2024-11-02 18:49:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        HELLO PLEASE WORK
+      </header>
+      <main>
+        this is the page content
+      </main>
+    </div>
+  );
+}
+
+export default App;
